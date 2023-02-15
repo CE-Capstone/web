ce20f5edcdef918e29222b4ab9a68e93855e6dc7
Joohyung Park <johy@dabyeol.com>
2023-02-15 18:29:38 +0900

    Implement Kakao login #3

--- src/components/LoginButton/KakaoLoginButton.tsx
+++ src/components/LoginButton/KakaoLoginButton.tsx
@@ -1,8 +1,18 @@
 import LoginButton from "./LoginButton";
 import KakaoIcon from "../../images/kakao.png";
+import KakaoLogin from "react-kakao-login";
 
 function KakaoLoginButton() {
-  return <LoginButton src={KakaoIcon} text="카카오 로그인" />;
+  return (
+    <KakaoLogin
+      token="adc3e6f5e05944b9f18bdcc9bef9295b"
+      onSuccess={console.log}
+      onFail={console.log}
+      render={({ onClick }) => (
+        <LoginButton onClick={onClick} src={KakaoIcon} text="카카오 로그인" />
+      )}
+    />
+  );
 }
 
 export default KakaoLoginButton;
