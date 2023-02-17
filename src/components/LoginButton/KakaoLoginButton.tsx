f8229931960ebb071b85c677d190cf8c9a2c681d
Joohyung Park <johy@dabyeol.com>
2023-02-17 22:37:43 +0900

    Add login test code

--- src/components/LoginButton/KakaoLoginButton.tsx
+++ src/components/LoginButton/KakaoLoginButton.tsx
@@ -1,13 +1,19 @@
 import LoginButton from "./LoginButton";
 import KakaoIcon from "../../images/kakao.png";
 import KakaoLogin from "react-kakao-login";
+import { useNavigate } from "react-router-dom";
 
 function KakaoLoginButton() {
+  const navigate = useNavigate();
+
   return (
     <KakaoLogin
       token="adc3e6f5e05944b9f18bdcc9bef9295b"
-      onSuccess={console.log}
-      onFail={console.log}
+      onSuccess={({ profile }) => {
+        navigate("/", { state: { profile } });
+      }}
+      onFail={console.error}
+      onLogout={console.info}
       render={({ onClick }) => (
         <LoginButton onClick={onClick} src={KakaoIcon} text="카카오 로그인" />
       )}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,15 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 function Home() {
+  const { state } = useLocation();
+
   return (
     <>
       <h1>Home</h1>
       <Link to="/login">Log in</Link>
+      <hr />
+      <h2>회원정보</h2>
+      {state && JSON.stringify(state)}
     </>
   );
 }
