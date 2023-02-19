20570c7be11527d16da5f4f05b64f7705b288f58
skjy1998 <skjy1998@gmail.com>
2023-02-19 16:32:31 +0900

    GoogleLogin

--- src/components/LoginButton/GoogleLoginButton.tsx
+++ src/components/LoginButton/GoogleLoginButton.tsx
@@ -1,16 +1,16 @@
-import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
+import { GoogleOAuthProvider, useGoogleLogin } from "@react-oauth/google";
+import GoogleIcon from "../../images/google.png";
+import LoginButton from "./LoginButton";
 
 function GoogleLoginButton() {
+  const GoogleLogin = useGoogleLogin({
+    onSuccess: (codeResponse) => console.log(codeResponse),
+    flow: "auth-code",
+  });
+
   return (
     <GoogleOAuthProvider clientId="670154900312-tqtanvlmi26hp9c453q22439qbfeiqsp.apps.googleusercontent.com">
-      <GoogleLogin
-        onSuccess={(credentialResponse) => {
-          console.log(credentialResponse);
-        }}
-        onError={() => {
-          console.log("Login Failed");
-        }}
-      />
+      <LoginButton onClick={GoogleLogin} src={GoogleIcon} text="구글 로그인" />
     </GoogleOAuthProvider>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId="670154900312-tqtanvlmi26hp9c453q22439qbfeiqsp.apps.googleusercontent.com">
+      <App />
+    </GoogleOAuthProvider>
   </React.StrictMode>
 );
