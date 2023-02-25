f29e4ef21916a1026ada6b56b36db3e1a0acc96b
Joohyung Park <johy@dabyeol.com>
2023-02-25 17:48:08 +0900

    Implement Google login #1

--- src/components/LoginButton/GoogleLoginButton.tsx
+++ src/components/LoginButton/GoogleLoginButton.tsx
@@ -1,10 +1,16 @@
 import { useGoogleLogin } from "@react-oauth/google";
+import { useNavigate } from "react-router-dom";
+import { getGoogleUserInfo } from "../../api";
 import LoginButton from "./LoginButton";
 
 function GoogleLoginButton() {
+  const navigate = useNavigate();
+
   const GoogleLogin = useGoogleLogin({
-    onSuccess: (codeResponse) => console.log(codeResponse),
-    flow: "auth-code",
+    onSuccess: async ({ access_token }) => {
+      const profile = await getGoogleUserInfo(access_token);
+      navigate("/", { state: { profile } });
+    },
   });
 
   return (
